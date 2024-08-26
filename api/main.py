9a88b56166798f5d43a4f8df1cbd2bc7b2f197f7
Ben Burwood <ben.burwood@streetwave.co>
2024-08-26 19:57:06 +0100

    Bugfixes and Cleanup

--- api/main.py
+++ api/main.py
@@ -1,17 +1,12 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 from api.PrinterManager import PrinterManager
-from api.config import get_font
+from api.print import PrintRequest
 from labelprinterkit.constants import Media
 from labelprinterkit.job import Job
-from labelprinterkit.labels.box import Box
-from labelprinterkit.labels.label import Label
-from labelprinterkit.labels.text import Padding, Text
 from labelprinterkit.printers.main import Printer
-from labelprinterkit.utils.font import get_fonts
 
 app = FastAPI()
 
@@ -24,18 +19,6 @@ async def lifespan(app: FastAPI):
 
 
 app.router.lifespan_context = lifespan
-
-
-class PrintRequest(BaseModel):
-    text: str
-    height: int
-    font: str = get_font()
-
-    @property
-    def label(self) -> Label:
-        text = Text(self.height, self.text, list(get_fonts().values())[0][0].as_posix(), padding=Padding(0, 10, 0, 0))
-        box = Box(70, text)
-        return Label(box)
 
 
 @app.get("/print")
---
+++ api/print.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+from api.config import get_font
+from labelprinterkit.labels.box import Box
+from labelprinterkit.labels.label import Label
+from labelprinterkit.labels.text import Padding, Text
+
+class PrintRequest(BaseModel):
+    text: str
+    height: int
+    font: str = get_font()
+    padding: Padding = Padding(0, 10, 0, 0)
+
+    @property
+    def label(self) -> Label:
+        text = Text(self.height, self.text, font_path=self.font, padding=self.padding)
+        box = Box(70, text)
+        return Label(box)
